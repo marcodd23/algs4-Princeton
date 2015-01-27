8e60441f964ee68b7a30519ec843ab071adfbf23
marco <marco.dd23@gmail.com>
2015-01-27 14:54:43 +0100

    collinear points project

---
+++ Collinear/Fast.java
@@ -0,0 +1,121 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author marco
+ */
+public class Fast {
+
+    public static void printPoints(Point pointArray[], int N, In inputFile) {
+        for (int i = 0; i < N; i++) {
+            int x = inputFile.readInt();
+            int y = inputFile.readInt();
+            pointArray[i] = new Point(x, y);
+            pointArray[i].draw();
+        }
+    }
+
+    public static boolean checkSubSegments(List<Point> discoveredStartSegments, List<Point> collinearPoints, double actualSlope) {
+        boolean subSegment = false;
+        if (!discoveredStartSegments.isEmpty()) {
+            for (Point point : discoveredStartSegments) {
+                if (point.slopeTo(collinearPoints.get(0)) == actualSlope) {
+                    subSegment = true;
+                }
+            }
+        }
+
+        return subSegment;
+    }
+
+    public static void printDiscoveredSegments(boolean subSegment, List<Point> collinearPoints) {
+
+        if (!subSegment) {
+            int size = collinearPoints.size();
+            for (int k = 0; k < size - 1; k++) {
+                StdOut.print(collinearPoints.get(k));
+                StdOut.print(" -> ");
+            }
+            StdOut.println(collinearPoints.get(size - 1) + "\n");
+            collinearPoints.get(0).drawTo(collinearPoints.get(size - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+
+        // make the points a bit larger
+        //StdDraw.setPenRadius(0.01);
+        Point pointArray[];
+
+        // read in the input
+        String filename = args[0];
+        In in = new In(filename);
+        int N = in.readInt();
+        pointArray = new Point[N];
+
+        printPoints(pointArray, N, in);
+
+        List<Point> discoveredStartSegments = new ArrayList<>();
+
+        double time;
+        StopwatchCPU stopWatch = new StopwatchCPU();
+
+        //Arrays.sort(pointArray);
+        for (int i = 0; i < N; i++) {
+
+            //Arrays.sort(pointArray);
+            Point p = pointArray[i];
+            Arrays.sort(pointArray, i + 1, N, p.SLOPE_ORDER);
+            //Arrays.sort(pointArray, i + 1, N);
+            //double prevSlope;
+            double actualSlope;
+            boolean subSegment;
+            List<Point> collinearPoints;
+            int j = i + 1;
+            while (j < N) {
+                actualSlope = pointArray[i].slopeTo(pointArray[j]);
+                collinearPoints = new ArrayList<>();
+                collinearPoints.add(pointArray[i]);
+                collinearPoints.add(pointArray[j]);
+
+                j = j + 1;
+                if (j < N && actualSlope == pointArray[i].slopeTo(pointArray[j])) {
+                    collinearPoints.add(pointArray[j]);
+                    j = j + 1;
+                    if (j < N && actualSlope == pointArray[i].slopeTo(pointArray[j])) {
+                        collinearPoints.add(pointArray[j]);
+                        j = j + 1;
+                        int scanIncr = j;
+                        for (int k = scanIncr; k < N; k++) {
+                            if (actualSlope == pointArray[i].slopeTo(pointArray[k])) {
+                                collinearPoints.add(pointArray[k]);
+                                j = k;
+                            } else {
+                                break;
+                            }
+                        }
+
+                        subSegment = checkSubSegments(discoveredStartSegments, collinearPoints, actualSlope);
+
+                        discoveredStartSegments.add(collinearPoints.get(0));
+                        Collections.sort(collinearPoints);
+
+                        printDiscoveredSegments(subSegment, collinearPoints);
+                    }
+                }
+            }
+        }
+
+        time = stopWatch.elapsedTime();
+        StdOut.println("\nTIME: " + time + " Seconds\n");
+
+    }
+
+}
+
---
+++ Collinear/Point.java
@@ -0,0 +1,120 @@
+import java.util.Comparator;
+
+/**
+ *
+ * @author marco
+ */
+public class Point implements Comparable<Point> {
+
+    public final int x;
+    public final int y;
+    public final Comparator<Point> SLOPE_ORDER;
+
+//    public final Comparator<Point> SLOPE_ORDER = new Comparator<Point>() {
+//
+//        @Override
+//        public int compare(Point point1, Point point2) {
+//            
+//            
+//            return 0;
+//        }
+//    };
+    
+    // compare points by slope to this point
+    private class SlopeOrderComparator implements Comparator<Point> {
+
+        @Override
+        public int compare(Point p1, Point p2) {
+            
+            double slopeToP1 = slopeTo(p1);
+            double slopeToP2 = slopeTo(p2);
+            
+            if(slopeTo(p1) > slopeTo(p2)){
+                return 1;
+            }else if(slopeTo(p1) < slopeTo(p2)){
+                return -1;
+            }else {
+                return 0;
+            }
+        }
+
+    }
+
+    public Point(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+        SLOPE_ORDER = new SlopeOrderComparator();
+    }
+
+    // draw this point
+    public void draw() {
+        StdDraw.point(x, y);
+    }
+
+    // draw the line segment from this point to that point
+    public void drawTo(Point that) {
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    // string representation
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    // Check if this point is lexicographically smaller than that point 
+    @Override
+    public int compareTo(final Point that) {
+        if (y < that.y || (y == that.y && x < that.x)) {
+            return -1;
+        }
+        if (y > that.y || (y == that.y && x > that.x)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    // the slope between this point and that point
+    public double slopeTo(final Point that) {
+        if (that == null) {
+            throw new NullPointerException();
+        }
+        if (x == that.x) {
+            if (y == that.y) {
+                return Double.NEGATIVE_INFINITY;
+            }
+            return Double.POSITIVE_INFINITY;
+        }
+        if (y == that.y){
+            return 0.0;
+        }
+        return ((double) (that.y - y)) / (that.x - x);
+    }
+
+    public static void main(String[] args) {
+        double a = 5.0;
+        double x = (a) / 0;
+        double y = (-a) / 0;
+        System.out.println(a < Double.POSITIVE_INFINITY);
+
+        System.out.println(a > Double.NEGATIVE_INFINITY);
+
+        double t = 1.000001;
+        double b = 0.000001;
+        double c = t - b;
+        System.out.println(c);
+        System.out.println(Math.abs(c - 1.0));
+        System.out.println(Math.abs(c - 1.0) <= 0.000001);
+        
+         System.out.println("\n ////////////////////////////////////\n");
+         
+         
+        int d = 9;
+        int f = 2;
+        System.out.println("-->" + ((double)d)/f + "\n" );
+        
+        
+        
+    }
+
+}
+
